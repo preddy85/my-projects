3468bfeac7e1acc7196c3148274d167a54b6e2c3
Reddy <preddy85@ms.ds.uhc.com>
2018-08-01 17:52:10 +0530

    new

---
+++ evenproject2/src/Evennumbers.java
@@ -0,0 +1,15 @@
+
+public class Evennumbers {
+	
+	 public static void main(String args[]){  
+	     int a=4;
+	     int b=2;
+	     int c;
+	     if(a%b==0)
+	     {
+		 System.out.println("Even........>>");
+	     }
+	     
+}
+}
+
